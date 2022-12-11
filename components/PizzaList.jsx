c1d86a1bece3cfe38f5c66381d30787b4fe4b86c
Mateus de Oliveira Pessoa <mateuspessoadev@gmail.com>
2022-12-11 00:08:20 -0300

    Adicionado botão para adicionar nova pizza

--- components/PizzaList.jsx
+++ components/PizzaList.jsx
@@ -4,6 +4,9 @@ import PizzaCard from "./PizzaCard"
 const PizzaList = () => {
   return (
     <div className={styles.container}>
+        <div className={styles.container_btn_add}>
+          <button className={styles.btn_add}>Adicionar Pizza</button>
+        </div>
         <h1 className={styles.title}>A MELHOR PIZZA DA REGIÃO</h1>
         <p className={styles.desc}>
             Aqui na nossa pizzaria você vai encotrar as pizzas mais saborosas
